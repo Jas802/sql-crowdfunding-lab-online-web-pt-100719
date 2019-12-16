1f1f5c293d18bdd1dcf010d0a20c5625099b4e81
Jas Spencer <jas.spencer414@gmail.com>
2019-12-16 02:45:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (id, name, age) VALUES (1, 'Jas', 26), (2, 'TA', 27), (3, 'Brad', 23), (4, 'Nathan', 29), (4, 'Forrest', 20), (5, 'Adam', 23),
-(6, 'Stuart', 20), (7, 'Ian', 14), (8, 'Sophie', 11), (9, 'Patrick', 25), (10, 'Masha', 23), (11, 'Jonathan', 60), (12, 'Becca', 26), 
+(6, 'Stuart', 20), (7, 'Ian', 14), (8, 'Sophie', 11), (9, 'Patrick', 25), (10, 'Masha', 23), (11, 'Jonathan', 60), (12, 'Becca', 26),
 (13, 'Auntie Murial', 107) (14, 'Percy Jackson', 16), (15, 'Luke Castellan, 19'), (16, 'Thalia Grace', 15), (17, 'Annabeth Chase', 15),
 (18, 'Grover Underwood', 28), (19, 'Mr. D', 3000), (20, 'Chiron', 3000);
