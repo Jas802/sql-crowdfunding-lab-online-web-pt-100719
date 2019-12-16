2fa4fd6500424eece6115330e1c4836037fd3a8d
Jas Spencer <jas.spencer414@gmail.com>
2019-12-16 02:55:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,16 @@ INSERT INTO users (id, name, age) VALUES (1, 'Jas', 26), (2, 'TA', 27), (3, 'Bra
 (7, 'Stuart', 20), (8, 'Ian', 14), (9, 'Sophie', 11), (10, 'Patrick', 25), (11, 'Masha', 23), (12, 'Jonathan', 60), (13, 'Becca', 26),
 (14, 'Auntie Murial', 107), (15, 'Percy Jackson', 16), (16, 'Luke Castellan', 19), (17, 'Thalia Grace', 15), (18, 'Annabeth Chase', 15),
 (19, 'Grover Underwood', 28), (20, 'Mr. D', 3000);
+
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Original Trilogy 4K', 'movies', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'I need a new laptop', 'charity', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'LOB 1st Edition box', 'hobby', 1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Culinary School', 'education', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Help with this lab!', 'education', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'Two-person straw', 'developement', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'Candy canes for every child!', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'Make it snow in the south', 'developement', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'Improve Patriot offense', 'education', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Knight Tony Romo', 'charity', 5000.00, '2012-03-20', '2013-06-30');
